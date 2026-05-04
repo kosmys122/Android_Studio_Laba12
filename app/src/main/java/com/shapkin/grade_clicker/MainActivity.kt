9814e895cad9fc242d70d84c332d9b69546c8267
Шапкин Николай <kosmys122@gmail.com>
2026-05-04 16:22:10 +0300

    tep 10: ifecycle callbacks and rememberaveable implemented

--- app/src/main/java/com/shapkin/grade_clicker/MainActivity.kt
+++ app/src/main/java/com/shapkin/grade_clicker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shapkin.grade_clicker
 import android.os.Bundle
 import com.shapkin.grade_clicker.GradeClickerApp
 import android.provider.ContactsContract
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -40,9 +41,11 @@ import model.Grade
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
+private  const val TAG="MainActivity"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate Called")
         enableEdgeToEdge()
         setContent {
             Grade_ClickerTheme {
@@ -53,6 +56,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy Called")
     }
 }
 
